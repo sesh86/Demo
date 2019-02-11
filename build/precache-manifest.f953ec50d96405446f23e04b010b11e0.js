8f04c2f427be7d4546260549184cf3b5d064bd20
Seshachalam S <seshachalam.s@pwc.com>
2019-02-11 16:23:01 +0530

    Testing build

---
+++ build/precache-manifest.f953ec50d96405446f23e04b010b11e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00b5fe657258f394c004",
+    "url": "/static/css/main.1fab11c3.chunk.css"
+  },
+  {
+    "revision": "00b5fe657258f394c004",
+    "url": "/static/js/main.00b5fe65.chunk.js"
+  },
+  {
+    "revision": "0f33a2f5950c7ebc0f92",
+    "url": "/static/js/1.0f33a2f5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4fbad139c5b6128157ad4536b5af43e8",
+    "url": "/index.html"
+  }
+];
