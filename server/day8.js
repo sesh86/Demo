d2171a78c40a87cfe846a62a754b54cf000b34d3
sesh86 <s.seshachalam@gmail.com>
2019-03-05 09:50:02 +0530

    day 8.1

--- server/day8.js
+++ server/day8.js
@@ -8,12 +8,18 @@ app=express();
 app.get('/',cors(),(req,res)=>{
     var user={id:1,user:'sesh'}
     jwt.sign(user,'secret',
-    // {expiresIn:60},
+    {expiresIn:10},
     (err,token)=>{
-      res.status(200).json(token);
       res.status(200).json(token);
     })
   });
+  app.get('/api/:token',cors(),(req,res)=>{
+    var decoded = jwt.decode(req.params.token);
+    if(decoded.exp<Date.now()){
+      res.send('Token Expired');
+    }
+    res.send(decoded);
+  })  
   app.listen(_app.port);
 
   console.log('Application is running on http://localhost:'+ _app.port);  
